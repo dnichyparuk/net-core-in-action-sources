8af027d733531628b2aae7aff96b9f860523bd02
dnichyparuk <dnichyparuk@gmail.com>
2019-11-28 15:11:54 +0300

    sample code sources

---
+++ README.md
@@ -0,0 +1,2 @@
+# .NET Core in Action
+Supplemental source code from [.NET Core in Action](https://www.manning.com/books/dotnet-core-in-action) by Manning Publishing
---
+++ chapter7/MarkdownService/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.DocAsCode.MarkdownLite;
+
+namespace MarkdownService
+{
+  public class Startup
+  {
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc();
+      
+      var builder = new GfmEngineBuilder(new Options());
+      var engine = builder.CreateEngine(new HtmlRenderer());
+      services.AddSingleton<IMarkdownEngine>(engine);
+
+      var configBuilder = new ConfigurationBuilder();
+      configBuilder.AddJsonFile("config.json", true);
+      var configRoot = configBuilder.Build();
+      services.AddSingleton<IConfigurationRoot>(configRoot);
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+      app.UseMvc();
+    }
+  }
+}
